380fd70808d95fb84b5b01ffc9818450869f50bb
scynscapa <607745+scynscapa@users.noreply.github.com>
2023-07-29 11:04:22 -0400

    Put grid in a fixed-size box

--- javascript.js
+++ javascript.js
@@ -1,6 +1,11 @@
+const GRID_SIZE = 400
+
 const container = document.querySelector('#container');
 
 function createGrid(sideSize) {
+    container.style.height = `${GRID_SIZE / sideSize}px`;
+    container.style.width = `${GRID_SIZE / sideSize}px`;
+
     if (sideSize > 100) {
         sideSize = 100;
     }
@@ -9,10 +14,13 @@ function createGrid(sideSize) {
         let row = document.createElement('div');
         row.className = 'row';
         row.style.display = 'flex';
+        row.style.height = `${GRID_SIZE / sideSize}px`;
 
         for (let j = 0; j < sideSize; j++) {
             let grid = document.createElement('div');
             grid.className = 'grid';
+            grid.style.height = `${GRID_SIZE / sideSize}px`;
+            grid.style.width = `${GRID_SIZE / sideSize}px`;
 
             row.appendChild(grid);
         }
@@ -30,9 +38,9 @@ function addMouseover() {
 }
 
 function clearGrid() {
-    const grids = document.querySelectorAll('.grid');
-    grids.forEach(grid => {
-        grid.remove();
+    const rows = document.querySelectorAll('.row');
+    rows.forEach(row => {
+        row.remove();
     });
 }
 
